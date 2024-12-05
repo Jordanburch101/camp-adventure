602cc176bd72f2e7e51adfb7b56e30c81dd45736
Jordan <jordanburch101@gmail.com>
2024-12-05 14:26:27 +1300

    updated eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
+  extends: [
+    'next/core-web-vitals',
+    'eslint:recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
     '@next/next/no-img-element': 'off'
