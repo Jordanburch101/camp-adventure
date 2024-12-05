16764383db5448f4ae00d338412afa9eb6565e00
Jordan <jordanburch101@gmail.com>
2024-12-05 14:47:01 +1300

    eslint changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,10 +9,10 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@next/next/no-img-element': 'off',
-    'no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
-    'react/no-unescaped-entities': 'off',
-    'no-redeclare': 'off'
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'react/react-in-jsx-scope': 'off'
+    }
   }
 } 
